5468ccd2952cec1ebfd6fbdfc0b976e8910ccd16
Anderson Alencar <andersonalencar@MacBook-Air-de-Anderson.local>
2020-01-30 19:05:11 -0300

    Start viewCode project

--- SwiftQuiz/SwiftQuiz/Controller/ViewController.swift
+++ SwiftQuiz/SwiftQuiz/Controller/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  SwiftQuiz
 //
-//  Created by Anderson Souza on 28/05/19.
-//  Copyright © 2019 Anderson Souza. All rights reserved.
+//  Created by Anderson Alencar on 30/01/20.
+//  Copyright © 2020 Anderson Alencar. All rights reserved.
 //
 
 import UIKit
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.modalPresentationStyle = .fullScreen
     }
 
 
---
+++ SwiftQuiz/SwiftQuiz/Utils/ViewCode.swift
@@ -0,0 +1,26 @@
+//
+//  ViewCode.swift
+//  SwiftQuiz
+//
+//  Created by Anderson Alencar on 30/01/20.
+//  Copyright © 2020 Anderson Alencar. All rights reserved.
+//
+
+import Foundation
+
+
+@objc protocol ViewCode {
+    
+    func buildHierarchy()
+    func setUpConstraints()
+    @objc optional func additionalConfigurations()
+}
+
+extension ViewCode{
+    
+    func SetUp(){
+        buildHierarchy()
+        setUpConstraints()
+        additionalConfigurations?()
+    }
+}
